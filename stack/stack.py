541f2a2ccb085e0e709d410c8b5636667f131d08
Rajesh Sharma <36369426+rajesh1998@users.noreply.github.com>
2019-01-29 14:55:28 +0530

    Update stack.py

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,4 @@
+# in this file all the basics functions of stack are defined.
 class ArrayStack:
     def __init__(self):
         #create an empty stack
@@ -5,21 +6,24 @@ class ArrayStack:
     def __len__(self):
         return len(self.data)
     def is_empty(self):
+        #true if it is empty
         return len(self.data)==0
     def push(self,e):
+        #inserts the element at topof the stack
         self.data.append(e)
     def top(self):
         if self.is_empty():
             raise Empty('stack is empty')
         return self.data[-1]
     def pop(self):
+        #to remove first element of the stack
         if self.is_empty():
             raise('stack is empty')
         return self.data.pop()
 
 
 '''
-S=ArrayStack
+S=ArrayStack()
 S.push(5)
 S.push(3)
 print(len(S))
